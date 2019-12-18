c6b365bdd9c879214ac4f882b97f715c3cb42bf9
Arielle Del Rocio Flores Del Castillo <arielle.fdelcastillo@alumnos.upm.es>
2019-12-18 17:04:35 +0100

    Cambiado entorno

--- JapanInterfaz.py
+++ JapanInterfaz.py
@@ -3,6 +3,7 @@ from JapanInterfazUI import Ui_MainWindow
 import Algoritmo
 import Grafo
 from VentanaAuxUI import Ui_MainWindow2
+from error import  Ui_MainWindow3
 
 #Creamos una clase para cada ventana: JapanInterfazUI y VentanaAuxUI
 class window(QMainWindow, Ui_MainWindow):
@@ -12,12 +13,27 @@ class window(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         #Al hacer click en el boton calcular ruta empezamos el dialogo entre las dos ventanas
         self.CalcularRuta.clicked.connect(self.on_PushButtonFirst_clicked)
-        self.partnerDialog = Window2(self)
 
     #Escribe en ruta el String devuelto por pressed
     def on_PushButtonFirst_clicked(self):
-        self.partnerDialog.ruta.setText(self.pressed())
+        if self.igual()!= True:
+            self.partnerDialog = Window2()
+            self.partnerDialog.ruta.setText(self.pressed())
+        else:
+            self.partnerDialog = Window3()
         self.partnerDialog.show()
+
+    '''
+    Devuelve un booleano dependiendo
+    si Origen y Destino son iguales.
+    '''
+    def igual(self):
+        x = self.comboOrigen.currentText()
+        y = self.comboDestino.currentText()
+        a=False
+        if x==y:
+            a=True
+        return a
 
     '''
     Devuelve un String con el camino calculado por
@@ -42,6 +58,15 @@ class Window2(QMainWindow, Ui_MainWindow2):
     def close_window2(self):
         self.close()
 
+class Window3(QMainWindow, Ui_MainWindow3):
+    def __init__(self, parent=None):
+        super(Window3, self).__init__(parent)
+        self.setupUi(self)
+        #Al hacer click en el boton ok cerramos la ventana
+        self.okBoton.clicked.connect(self.close_window3)
+
+    def close_window3(self):
+        self.close()
 
 if __name__ == "__main__":
     app = QApplication([])
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/diego/PycharmProjects/IA/env"
+set -gx VIRTUAL_ENV "/home/arielle/PycharmProjects/IA/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
